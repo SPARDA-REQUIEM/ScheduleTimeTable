b4ee9ec8b74fa591a16e1c04d229a37458f8443e
SPARDA-REQUIEM <patrickbuyain@gmail.com>
2022-11-16 17:56:07 +0800

    modified:   MainApp/Templates/add.html 	modified:   MainApp/Templates/base.html 	modified:   MainApp/__pycache__/models.cpython-311.pyc 	modified:   MainApp/migrations/0001_initial.py 	modified:   MainApp/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   MainApp/models.py 	modified:   db.sqlite3

--- MainApp/models.py
+++ MainApp/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class facultyMember(models.Model):
     inputId = models.CharField(max_length = 10)
     inputName = models.CharField(max_length = 100)
-    inputDepartment = models.CharField(max_length = 100)
     inputEmploymentStatus = models.CharField(max_length = 100)
+    inputCollege = models.CharField(max_length = 100)
+    inputDepartment = models.CharField(max_length = 100)
     inputExpertiseSubject = models.CharField(max_length = 300) #Checkbox 
     inputDayAvailability = models.CharField(max_length = 100) #Checkbox With InputField Below | Calendar
     inputTimePreference = models.CharField(max_length = 100) #Checkbox With InputField Below | Calendar
