503102b3c4e5272ad5e6027da6272d31956a4619
SPARDA-REQUIEM <patrickbuyain@gmail.com>
2022-11-16 09:14:21 +0800

    modified:   MainApp/Templates/add.html 	modified:   MainApp/__pycache__/models.cpython-311.pyc 	modified:   MainApp/migrations/0001_initial.py 	modified:   MainApp/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   MainApp/models.py 	modified:   ScheduleTimeTable/__pycache__/urls.cpython-311.pyc 	modified:   db.sqlite3

--- MainApp/models.py
+++ MainApp/models.py
@@ -4,7 +4,12 @@ from django.db import models
 class facultyMember(models.Model):
     inputId = models.CharField(max_length = 10)
     inputName = models.CharField(max_length = 100)
-    inputContactNumber = models.CharField(max_length = 11)
+    inputDepartment = models.CharField(max_length = 100) #Choicefield
+    inputEmploymentStatus = models.CharField(max_length = 100) #Choicefield
+    inputExpertiseSubject = models.CharField(max_length = 100) #Checkbox
+    inputDayAvailability = models.CharField(max_length = 100) #Checkbox
+    inputTimePreference = models.CharField(max_length = 100) #Choicefield
+    inputUnitsLoad = models.CharField(max_length = 100)
 
     def __str__(self):
         return self.inputName
